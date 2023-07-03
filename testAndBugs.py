a7456fe0dcbd86acb2ba761d5aadebb8cfbac218
Katrin Marques Magalhaes <katrinmarques@gmail.com>
2023-07-03 13:53:34 +0200

    drop duplicates for the company profile pages

--- testAndBugs.py
+++ testAndBugs.py
@@ -296,12 +296,133 @@ def detail_info(start, end,jd ,seniority,emp_type,job_func,job_ind,prof,rand_job
 detail_timestart= datetime.datetime.now()
 jd,seniority,emp_type, job_func,job_ind ,prof =detail_info(start=0, end=10, jd=[], seniority=[],emp_type=[],job_func=[], job_ind=[],prof=[])
 
-jd,seniority,emp_type, job_func,job_ind ,prof =detail_info(start=10, end=len(rand_jobs))
+jd,seniority,emp_type, job_func,job_ind ,prof =detail_info(start=20,jd=jd, seniority=seniority,emp_type=emp_type, job_func=job_func, job_ind=job_ind, prof=prof,rand_jobs=rand_jobs, end=len(rand_jobs))
 detail_timeend=datetime.datetime.now() 
 
 time_detailinfo= detail_timeend-detail_timestart
 time_detailinfo
 
+
+dataMerge= pd.DataFrame({
+    'Date': date_list,
+    'Company': company_name_list,
+    'Title': job_title_list,
+    'Location': location_list,
+    'Link': job_link_list,
+    'Description': jd,
+    'Level': seniority,
+    'Type': emp_type,
+    'Function': job_func,
+    'Industry': job_ind,
+    'profile_Link': prof
+    #'Profile': prof_text,
+    #'Company_Size': comp_size
+})
+dataMerge.to_csv(r"df0307_{0}_{1}_{2}_{3}.csv".format(job_name,jobs_num,ort,erfahrung))
+dataMerge.to_pickle(r"df0307_{0}_{1}_{2}_{3}.pkl".format(job_name,jobs_num,ort,erfahrung))
+
+####################
+#
+#Check for one detailed info if there is 'Loading took too much time'
+#
+###################
+
+item=rand_jobs[5]
+
+num= jobs.index(item) # not rand_jobs, because the order changed there!
+print(num)
+#job_func0=[]
+#industries0=[]
+# clicking job to view job details
+
+#__________________________________________________________________________ JOB Link
+
+try: 
+    job_click_path = f'/html/body/div[1]/div/main/section[2]/ul/li[{num+1}]'
+    #Wait as long as required, or maximum 10 sec before for the page loading of the detailed job description on the right side of the page
+    element= WebDriverWait(driver= driver, timeout=20).until(EC.presence_of_element_located((By.XPATH, job_click_path)))
+    time.sleep(random.randint(2,4)) # to ensure that the scrolling is not faster than my code on saving the data 
+    element.click() 
+
+
+    #job_click = item.find_element(By.XPATH,job_click_path).click() # The URL changes when clicking on a certain job offer
+
+    # random waiting time to avoid a certain structure,so I do not get banned
+    #job_click = item.find_element(By.XPATH,'.//a[@class="base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]"]')
+except TimeoutException:
+    print(r"Loading took too much time")
+    pass
+
+
+#__________________________________________________________________________ JOB Description
+jd_path = '/html/body/div/div/section/div/div/section/div/div/section/div'
+
+try:
+
+    jd0 = item.find_element(By.XPATH,jd_path).get_attribute('innerText')
+    jd.append(jd0)
+except:
+    jd.append(None)
+    pass
+
+#__________________________________________________________________________ JOB Seniority
+seniority_path='/html/body/div/div/section/div/div/section/div/ul/li[1]/span'
+
+try:
+    seniority0 = item.find_element(By.XPATH,seniority_path).get_attribute('innerText')
+    seniority.append(seniority0)
+except:
+    seniority.append(None)
+    pass
+
+#__________________________________________________________________________ JOB Time
+emp_type_path='/html/body/div/div/section/div/div/section/div/ul/li[2]/span'
+
+try:
+    emp_type0 = item.find_element(By.XPATH,emp_type_path).get_attribute('innerText')
+    emp_type.append(emp_type0)
+except:
+    emp_type.append(None)
+    pass
+
+#__________________________________________________________________________ JOB Function
+function_path='/html/body/div/div/section/div/div/section/div/ul/li[3]/span'
+
+try:
+    func0 = item.find_element(By.XPATH,function_path).get_attribute('innerText')
+    job_func.append(func0)
+except:
+    job_func.append(None)
+    pass
+
+#__________________________________________________________________________ JOB Industry
+industry_path='/html/body/div/div/section/div/div/section/div/ul/li[4]/span'
+
+try:
+    ind0 = item.find_element(By.XPATH,industry_path).get_attribute('innerText')
+    job_ind.append(ind0)
+except:
+    job_ind.append(None)
+    pass
+
+# Den Path Finden: rechtklick auf das gewünschte Objekt/ den Text >> untersuchen, 
+# dann rechtsklick auf die markierte Stelle im HTML Code >> kopieren >> gesamten XPATH kopieren
+profile_link_path = '/html/body/div[1]/div/section/div[2]/section/div/a'
+# old that worked on other file one_job : '/html/body/div[3]/div/section/div[2]/section/div/a'
+#f'/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/h4/div[1]/span[1]/a' #vorher anfang april
+#second: /html/body/div[3]/div/section/div[2]/section/div/a
+
+
+try:
+    prof0 = item.find_element(By.XPATH, profile_link_path).get_attribute('href')
+    prof0
+    prof.append(prof0)
+    
+except:
+    prof.append(None)
+    pass
+detail_timeend=datetime.datetime.now() 
+#time.sleep(2)
 
 ##############################################################################################################
 #
@@ -311,8 +432,22 @@ time_detailinfo
 
 #driver.back()
 #driver.close()
-unique_prof= [*set(prof)]
-len(unique_prof)
+# Only webscrape the profile page of a company ONCE. One company often has multiple job offers
+
+df= pd.DataFrame([prof,rand_jobs]).transpose()
+u_prof= [*set(df[0])]
+
+unique=df.drop_duplicates(subset=[0], keep='first')
+
+#unique_prof= [*set(prof)]
+#len(unique_prof) # for 1 example 329.. that is one third! a lot of time saving by avoiding duplicates
+
+unique_webelem=[*set(rand_jobs)]
+len(unique_webelem)
+rand_jobs[0]
+prof[0]
+
+
 driver.current_window_handle
 driver.switch_to.new_window()
 # Lets open https://www.facebook.com/ in the third tab
@@ -330,14 +465,25 @@ driver.switch_to.window('thirdtab')
 driver.get(prof[0])
 driver.close()
 
+#close tabs that are not needed anymore, all except the first tab
+for handle in driver.window_handles[1:]:
+    driver.switch_to.window(handle)
+    driver.close()
+
+driver.switch_to.window(driver.window_handles[0])
+
 prof_text=[]
 comp_size=[]
 
-#item=rand_jobs[2]
+item=rand_jobs[0]
 
-for item in rand_jobs[0:20]: #range(len(jobs)):
+maxtab=10
+i=0
+
+for item in rand_jobs[0:10]: #range(len(jobs)):
     num= jobs.index(item) # not rand_jobs, because the order changed there!
     print(num)
+    i=+1
 
     #__________________________________________________________________________ JOB Link
     
@@ -391,3 +537,8 @@ for item in rand_jobs[0:20]: #range(len(jobs)):
     #driver.back()
     driver.switch_to.window(tabs[0])
     time.sleep(1)
+    if item in rand_jobs[0::10]:
+        for handle in driver.window_handles[1:]:
+            driver.switch_to.window(handle)
+            driver.close()
+
